ec2272fc5a6ffe39fa58b96f7eff22449c699a8e
PatrickPLG <44425816+PatrickPLG@users.noreply.github.com>
2020-05-29 09:53:39 +0200

    hp

--- SimplePlatformGame/Fire.gd
+++ SimplePlatformGame/Fire.gd
@@ -1,12 +1,25 @@
 #WorldComplete.gd
 extends Area2D
+onready var health = get_node("../Player").health
+var timer = null
+var can_damage = true
 
-var health = 100
+func _ready():
+	timer = Timer.new()
+	timer.set_one_shot(true)
+	timer.set_wait_time(0.2)
+	timer.connect("timeout", self, "on_timeout_complete")
+	add_child(timer)
 
-# warning-ignore:unused_argument
 func _physics_process(delta):
+	pass
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
-		if body.name == "Player":
-			health = health - 1
+		if body.name == "Player" and can_damage:
+			health = health - 5
 			print(health)
+			can_damage = false
+			timer.start()
+
+func on_timeout_complete():
+	can_damage = true
